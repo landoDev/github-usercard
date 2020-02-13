909633c4cddc6fd119e52a815d1963ff1ee8949e
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-13 15:56:25 -0500

    step four done. api calls baby, its lit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,10 +2,11 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-// axios.get('https://api.github.com/users/landoDev')
-//   .then (response => {
-//     console.log(response)
-// })
+axios.get('https://api.github.com/users/landoDev')
+  .then (response => {
+    cardEntry.append(gitUserCard(response.data));
+    // console.log(response.data);
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -16,7 +17,7 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
-
+const cardEntry = document.querySelector('.cards');
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -100,6 +101,10 @@ function gitUserCard(dataObj) {
   return gitCard;
 }
 // console.log(gitUserCard());
+// ===== Passing API call into function ===== //
+
+
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
